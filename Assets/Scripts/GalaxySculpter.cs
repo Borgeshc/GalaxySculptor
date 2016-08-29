e890a7b4c032aa0ef76ef52c81d0c6c352fc30ec
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-08-29 16:58:08 -0500

    Added Planets!

---
+++ Assets/Scripts/GalaxySculpter.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GalaxySculpter : MonoBehaviour
+{
+    public List<GameObject> planets;    //List of planets.
+    public float planetMinSize;   //Min size a planet could be.
+    public float planetMaxSize;   //Max size a planet could be.
+    
+    public float planetsMinX; //Max for the X a planet could spawn.
+    public float planetsMaxX; //Min for the X a planet could spawn.
+    public float planetsMinY; //Min for the Y a planet could spawn.
+    public float planetsMaxY; //Max for the Y a planet could spawn.
+    public float planetsMinZ; //Min for the Z a planet could spawn.
+    public float planetsMaxZ; //Max for the Z a planet could spawn.
+
+    public List<Vector3> occupiedPlanetPositions; //The positons of spawned planets.
+    private Vector3 desiredPlanetPosition; //Where the planet is trying to spawn.
+
+    [HideInInspector]
+    public string[] rotationOptions = new string[] { "rotateUp", "rotateDown", "rotateLeft", "RotateRight" };   //Options for the rotation directions
+    [HideInInspector]
+    public string rotationDirection;    //The choosen rotation.
+
+    public float planetsMinRotationSpeed;  //A planets minimum rotation speed.
+    public float plnaetsMaxRotationSpeed;  //A planets maximum rotation speed.
+
+    void Start ()
+    {
+        Planets();
+    }
+	void Planets ()
+    {
+       
+        foreach (GameObject planet in planets)  //For every planet in the array named planet.
+        {
+            Instantiate(planet, transform.position, transform.rotation);//Create the planet.
+            occupiedPlanetPositions.Add(planet.transform.position);
+            //This will pick a random Vector3 to spawn the planet on.
+            desiredPlanetPosition = new Vector3(Random.Range(planetsMinX, planetsMaxX), Random.Range(planetsMinY, planetsMaxY), Random.Range(planetsMinZ, planetsMaxZ));
+            for (int i = 0; i < occupiedPlanetPositions.Count; i++)
+            {
+                if (!occupiedPlanetPositions.Contains(desiredPlanetPosition))    //If the desiredPosition of the plnaet is not on another planet
+                {
+                    planet.transform.position = desiredPlanetPosition;  //Move the planet to the desired position
+                    occupiedPlanetPositions.Add(planet.transform.position); //Add the planets new position to the list
+                    i = occupiedPlanetPositions.Count; //End the loop
+                }
+                else
+                    //if the desired position is on top of a planet, choose a different position.
+                    desiredPlanetPosition = new Vector3(Random.Range(planetsMinX, planetsMaxX) , Random.Range(planetsMinY, planetsMaxY), Random.Range(planetsMinZ, planetsMaxZ));
+            }
+            planet.tag = "Planet";
+            planet.AddComponent<ObjectRotation>();  //Add the rotation script to the gameobject.
+            //Scale the gameObject according to public variables
+            planet.transform.localScale = new Vector3 (Random.Range(planetMinSize, planetMaxSize), Random.Range(planetMinSize, planetMaxSize), Random.Range(planetMinSize, planetMaxSize));
+            rotationDirection = rotationOptions[Random.Range(0, rotationOptions.Length)];   //Choose a random rotation for the planet.
+
+            //Place the gameObject randomly in the scene.
+           
+            
+        }
+	}
+}
---
+++ Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotation : MonoBehaviour
+{
+    [HideInInspector]
+    public float rotationSpeed; //How fast does the object rotate
+    public Vector3 rotationDirection;   //This will change depending on which bool you check.
+    public bool rotateUp;   //Rotate object up.
+    public bool rotateDown; //Rotate object down.
+    public bool rotateLeft; //Rotate object left.
+    public bool rotateRight;    //Rotate object right.
+
+    GameObject gameManager; //Reference the gameManager object.
+    GalaxySculpter galaxySculpter;  //Reference to the GalaxySculpter script.
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");   //Find the GameManager object.
+        galaxySculpter = gameManager.GetComponent<GalaxySculpter>();    //Get the GalaxySculpter component on the gameManager object.
+        rotationSpeed = Random.Range(galaxySculpter.planetsMinRotationSpeed, galaxySculpter.plnaetsMaxRotationSpeed);
+        //Set the rotation of this object based upon the values given in the GalaxySculpter script.
+
+        switch (galaxySculpter.rotationDirection)   //Set the rotation of the object based upon the string choosen in the GalaxySculpter script
+        {
+            case "rotateUp":
+                rotationDirection = Vector3.up;
+            break;
+            case "rotateDown":
+                rotationDirection = Vector3.down;
+                break;
+            case "rotateLeft":
+                rotationDirection = Vector3.left;
+                break;
+            case "rotateRight":
+                rotationDirection = Vector3.right;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        transform.Rotate(rotationDirection * rotationSpeed * Time.deltaTime);   //Object rotates based upon the bool chosen * the speed.
+    }
+}
