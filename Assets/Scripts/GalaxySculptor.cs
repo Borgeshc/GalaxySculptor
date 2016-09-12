2414a4a02efdc3535eddbc06c133b888470d4802
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-09-12 12:09:17 -0500

    Added logic for moons

--- Assets/Scripts/GalaxySculptor.cs
+++ Assets/Scripts/GalaxySculptor.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 public class GalaxySculptor : MonoBehaviour
 {
+
+    //Planets
+
     public List<GameObject> planets;    //List of planets.
     public float planetMinSize;   //Min size a planet could be.
     public float planetMaxSize;   //Max size a planet could be.
@@ -23,24 +26,77 @@ public class GalaxySculptor : MonoBehaviour
     public string rotationDirection;    //The choosen rotation.
 
     public float planetsMinRotationSpeed;  //A planets minimum rotation speed.
-    public float plnaetsMaxRotationSpeed;  //A planets maximum rotation speed.
+    public float planetsMaxRotationSpeed;  //A planets maximum rotation speed.
     private Vector3 desiredPlanetPosition; //Where the planet is trying to spawn.
     private float sizeChosen;
+
+
+    //Moons
+    public List<GameObject> moons;    //List of planets.
+    public float moonMinSize;   //Min size a planet could be.
+    public float moonMaxSize;   //Max size a planet could be.
+
+    public float moonsMinX; //Max for the X a planet could spawn.
+    public float moonsMaxX; //Min for the X a planet could spawn.
+    public float moonsMinY; //Min for the Y a planet could spawn.
+    public float moonsMaxY; //Max for the Y a planet could spawn.
+    public float moonsMinZ; //Min for the Z a planet could spawn.
+    public float moonsMaxZ; //Max for the Z a planet could spawn.
+
+    public List<Vector3> occupiedMoonPositions; //The positons of spawned planets.
+
+    public float moonMinRotationSpeed;  //A planets minimum rotation speed.
+    public float moonMaxRotationSpeed;  //A planets maximum rotation speed.
+    private Vector3 desiredMoonPosition; //Where the planet is trying to spawn.
+
+    public List<GameObject> activePlanets;
 
     void Start ()
     {
         Planets();
+        Moons();
     }
-	void Planets ()
+	void Moons ()
     {
-       
+        foreach (GameObject moon in moons)  //For every moon in the array named planet.
+        {
+            GameObject clone = Instantiate(moon, transform.position, transform.rotation) as GameObject; //Create the moon.
+            clone.transform.SetParent(activePlanets[Random.Range(0, activePlanets.Count)].transform);
+            occupiedMoonPositions.Add(clone.transform.position);
+            //This will pick a random Vector3 to spawn the moon on.
+            desiredMoonPosition = new Vector3(Random.Range(moonsMinX, moonsMaxX), Random.Range(moonsMinY, moonsMaxY), Random.Range(moonsMinZ, moonsMaxZ));
+            for (int i = 0; i < occupiedMoonPositions.Count; i++)
+            {
+                if (!occupiedMoonPositions.Contains(desiredMoonPosition))    //If the desiredPosition of the moon is not on another moon
+                {
+                    clone.transform.position = desiredMoonPosition;  //Move the moon to the desired position
+                    occupiedMoonPositions.Add(clone.transform.position); //Add the moon new position to the list
+                    i = occupiedMoonPositions.Count; //End the loop
+                }
+                else
+                    //if the desired position is on top of a moon, choose a different position.
+                    desiredMoonPosition = new Vector3(Random.Range(moonsMinX, moonsMaxX) , Random.Range(moonsMinY, moonsMaxY), Random.Range(moonsMinZ, moonsMaxZ));
+            }
+            clone.tag = "Moon";
+            clone.AddComponent<ObjectRotation>();  //Add the rotation script to the gameobject.
+            //Scale the gameObject according to public variables
+            sizeChosen = Random.Range(moonMinSize, moonMaxSize);    //Chooses a random size based upon the min and max sizes chosen.
+            clone.transform.localScale = new Vector3 (sizeChosen,sizeChosen,sizeChosen); //Scales the gameobject to that size.
+            rotationDirection = rotationOptions[Random.Range(0, rotationOptions.Length)];   //Choose a random rotation for the moon.
+
+
+            //Place the gameObject randomly in the scene.
+        }
+	}
+    void Planets()
+    {
         foreach (GameObject planet in planets)  //For every planet in the array named planet.
         {
             GameObject clone = Instantiate(planet, transform.position, transform.rotation) as GameObject;//Create the planet.
+            activePlanets.Add(clone);
             occupiedPlanetPositions.Add(clone.transform.position);
             //This will pick a random Vector3 to spawn the planet on.
             desiredPlanetPosition = new Vector3(Random.Range(planetsMinX, planetsMaxX), Random.Range(planetsMinY, planetsMaxY), Random.Range(planetsMinZ, planetsMaxZ));
-            print(desiredPlanetPosition);
             for (int i = 0; i < occupiedPlanetPositions.Count; i++)
             {
                 if (!occupiedPlanetPositions.Contains(desiredPlanetPosition))    //If the desiredPosition of the plnaet is not on another planet
@@ -51,18 +107,16 @@ public class GalaxySculptor : MonoBehaviour
                 }
                 else
                     //if the desired position is on top of a planet, choose a different position.
-                    desiredPlanetPosition = new Vector3(Random.Range(planetsMinX, planetsMaxX) , Random.Range(planetsMinY, planetsMaxY), Random.Range(planetsMinZ, planetsMaxZ));
+                    desiredPlanetPosition = new Vector3(Random.Range(planetsMinX, planetsMaxX), Random.Range(planetsMinY, planetsMaxY), Random.Range(planetsMinZ, planetsMaxZ));
             }
             clone.tag = "Planet";
             clone.AddComponent<ObjectRotation>();  //Add the rotation script to the gameobject.
             //Scale the gameObject according to public variables
             sizeChosen = Random.Range(planetMinSize, planetMaxSize);    //Chooses a random size based upon the min and max sizes chosen.
-            clone.transform.localScale = new Vector3 (sizeChosen,sizeChosen,sizeChosen); //Scales the gameobject to that size.
+            clone.transform.localScale = new Vector3(sizeChosen, sizeChosen, sizeChosen); //Scales the gameobject to that size.
             rotationDirection = rotationOptions[Random.Range(0, rotationOptions.Length)];   //Choose a random rotation for the planet.
-
-            //Place the gameObject randomly in the scene.
-           
-            
+           //clone.GetComponent<ObjectRotation>().ChooseRotation(clone);
+           //Place the gameObject randomly in the scene.
         }
-	}
+    }
 }
--- Assets/Scripts/ObjectRotation.cs
+++ Assets/Scripts/ObjectRotation.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class ObjectRotation : MonoBehaviour
 {
     [HideInInspector]
-    public float rotationSpeed; //How fast does the object rotate
+    public float planetRotationSpeed; //How fast does the object rotate
+    [HideInInspector]
+    public float moonRotationSpeed;
     [HideInInspector]
     public Vector3 rotationDirection;   //This will change depending on which bool you check.
     bool rotateUp;   //Rotate object up.
     bool rotateDown; //Rotate object down.
     bool rotateLeft; //Rotate object left.
     bool rotateRight;    //Rotate object right.
+
+    bool isPlanet;
+    bool isMoon;
 
     private string rotationChosen;
 
@@ -21,7 +26,8 @@ public class ObjectRotation : MonoBehaviour
     {
         gameManager = GameObject.Find("GameManager");   //Find the GameManager object.
         galaxySculptor = gameManager.GetComponent<GalaxySculptor>();    //Get the GalaxySculpter component on the gameManager object.
-        rotationSpeed = Random.Range(galaxySculptor.planetsMinRotationSpeed, galaxySculptor.plnaetsMaxRotationSpeed);
+        planetRotationSpeed = Random.Range(galaxySculptor.planetsMinRotationSpeed, galaxySculptor.planetsMaxRotationSpeed);
+        moonRotationSpeed = Random.Range(galaxySculptor.moonMinRotationSpeed, galaxySculptor.moonMaxRotationSpeed);
         rotationChosen = galaxySculptor.rotationDirection;
         //Set the rotation of this object based upon the values given in the GalaxySculpter script.
 
@@ -29,7 +35,7 @@ public class ObjectRotation : MonoBehaviour
         {
             case "rotateUp":
                 rotationDirection = Vector3.up;
-            break;
+                break;
             case "rotateDown":
                 rotationDirection = Vector3.down;
                 break;
@@ -40,10 +46,23 @@ public class ObjectRotation : MonoBehaviour
                 rotationDirection = Vector3.right;
                 break;
         }
+
+        switch (transform.tag)
+        {
+            case "Planet":
+                isPlanet = true;
+                break;
+            case "Moon":
+                isMoon = true;
+                break;
+        }
     }
 
     void Update()
     {
-        transform.Rotate(rotationDirection * rotationSpeed * Time.deltaTime);   //Object rotates based upon the bool chosen * the speed.
+        if(isPlanet)
+            transform.Rotate(rotationDirection * planetRotationSpeed * Time.deltaTime);   //Object rotates based upon the bool chosen * the speed.
+        if(isMoon)
+            transform.Rotate(rotationDirection * moonRotationSpeed * Time.deltaTime);   //Object rotates based upon the bool chosen * the speed.
     }
 }
